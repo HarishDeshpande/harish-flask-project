914ac3ffb54d02072c64368b3eaddcb78ab3d86e
HarishDeshpande <79131107+HarishDeshpande@users.noreply.github.com>
2024-01-12 07:00:55 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # "/" for the default page or empty page
 @app.route("/")
 def hello_world():
-  return "Namaste Harish"
+  return render_template('home.html')
 
 # here if the script is invoked using the python command
 # then the app will be run
